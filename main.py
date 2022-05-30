380c6bf906ee4745d589efd56c90094227b2fdc8
Yisaas <jesusmunozpost@gmail.com>
2022-05-30 22:05:16 +0200

    Hi

---
+++ main.py
@@ -0,0 +1,20 @@
+# import random
+# print (random.randint (1, 10))
+
+#Importamos la biblioteca entera de RANDOM
+# import random
+
+#Importamos el aparatado de randint de la biblioteca RANDOM
+# from random import randint
+
+#Importamos la biblioteca entera de RANDOM
+#from random import*
+"Nos permite codificar sin tener que emplear ramdom.randint"
+from random import*
+print (randint (1, 10))
+
+
+# import random
+# choices = ["piedra", "papel" "tijera"]
+# print (random.choice(choices))
+
